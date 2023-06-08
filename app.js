ad5e6a8116eb2198854f98f050490395bf2b1dc7
jduran2101 <jhon21720@gmail.com>
2023-06-08 18:35:18 -0300

    arreglo errores de imagen en el html

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ let htmlpath = path.join(__dirname, './views/home.html')
 app.get('/',(req,res)=>{
     res.sendFile(htmlpath)
 });
+let loginpath = path.join(__dirname, './views/login.html')
+app.get('/login',(req,res)=>{
+    res.sendFile(loginpath)
+});
 app.use(express.static('public'));
